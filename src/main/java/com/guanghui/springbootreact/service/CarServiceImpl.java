de3a3ad0ac984b14b8a333a0a867f957a5c9131b
Guanghui Wang <guanghui-wang@foxmail.com>
2022-05-02 21:32:22 +0800

    refactor: CarServiceImpl rename

--- src/main/java/com/guanghui/springbootreact/service/CarServiceImpl.java
+++ src/main/java/com/guanghui/springbootreact/service/CarServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CarServiceImp implements CarService {
+public class CarServiceImpl implements CarService {
     @Autowired
     private CarRepository carRepository;
 
