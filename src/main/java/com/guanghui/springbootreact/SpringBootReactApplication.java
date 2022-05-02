3c875d4e1a68198b729e583203ef7dddfdeb9778
Guanghui Wang <guanghui-wang@foxmail.com>
2022-05-02 13:11:14 +0800

    feat: logger

--- src/main/java/com/guanghui/springbootreact/SpringBootReactApplication.java
+++ src/main/java/com/guanghui/springbootreact/SpringBootReactApplication.java
@@ -1,13 +1,18 @@
 package com.guanghui.springbootreact;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootReactApplication {
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootReactApplication.class);
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactApplication.class, args);
+        logger.info("Application started");
     }
 
 }
